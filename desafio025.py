5d2fd2b1302d7a2503d8b947fdc7664ea520e58a
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:43:55 -0300

    Update desafio025.py

--- desafio025.py
+++ desafio025.py
@@ -1,2 +1,6 @@
+'''
+Desafio 025: Crie um programa que leia o nome de uma pessoa e diga se ela tem “SILVA” no nome.
+'''
+
 nome = str(input('Digite seu nome completo: ')).strip()
 print('O nome possui Silva?: {}'.format('silva' in nome.lower()))
