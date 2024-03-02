b96ef2e186eabc3f25980f559924caeb88b277e7
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 19:16:13 -0300

    Update desafio060-B.py

--- desafio060-B.py
+++ desafio060-B.py
@@ -1,9 +1,16 @@
+'''
+Desafio 060: Faça um programa que leia um número qualquer e mostre o seu fatorial, exemplo: 5! = 5x4x3x2x1 = 120.
+>>> Usando FOR, sem biblíoteca:
+''' 
+
 print('\033[35m-=-' * 15)
 print(f'{' ':>10}CALCULANDO O FATORIAL')
 print('-=-' * 15)
+
 num = int(input('\033[mDigite um número para calcular seu Fatorial: '))
 fat = 1    # recebe 1 para fazer uma multiplicação limpa.
 print(f'Contando...{num}! = ', end=' ')
+
 for f in range(num, 0, -1):
     print(f'{f}', end=' ')
     print(' x ' if f > 1 else ' = ', end=' ')
