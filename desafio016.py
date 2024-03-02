ef408294c88a5dc0762af442afd93193fd7aa11b
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:37:45 -0300

    Update desafio016.py

--- desafio016.py
+++ desafio016.py
@@ -1,3 +1,7 @@
+'''
+Desafio 016: Crie um programa que leia um número real qualquer pelo teclado e mostre na tela a sua porção inteira.
+'''
+
 from math import trunc
 num = float(input('Informe um número real: '))
 print('O número {} tem a parte inteira {}'.format(num, trunc(num)))
