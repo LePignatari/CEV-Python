60061701e17848e7683ddf8c8ac1c4fb84d31037
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:57:13 -0300

    Update desafio040.py

--- desafio040.py
+++ desafio040.py
@@ -1,3 +1,8 @@
+'''
+Desafio 040: Crie um programa que leia duas notas de um aluno e calcule sua média, mostrando uma mensagem no final, de acordo com a média atingida: 
+→ Média abaixo de 5.0: reprovado, média entre 5.0 e 6.9: recuperação, média 7.0 ou +: aprovado.
+'''
+
 print('-=-'*5)
 n1 = float(input('Primeira nota: '))
 n2 = float(input('Segunda nota: '))
