64bfda449d11b601357e1d060a44fc3052cb8cc7
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 19:08:09 -0300

    Update desafio053-B.py

--- desafio053-B.py
+++ desafio053-B.py
@@ -1,3 +1,8 @@
+'''
+Desafio 053 - Crie um programa que leia uma frase qualquer e diga se ela é um palíndromo (frases que podem ser lidas de trás para frente, ex: após a sopa.), desconsiderando os espaços.
+>>> Usando o for:
+'''
+
 frase = str(input('Escreva uma frase: ')).strip().split()
 frase_junta = ''.join(frase).upper()
 inverte = ''
