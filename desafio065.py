254234e3dd3cabbff4913eff0c8a83b6243ad08e
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 19:20:29 -0300

    Update desafio065.py

--- desafio065.py
+++ desafio065.py
@@ -1,3 +1,9 @@
+'''
+Desafio 065: Crie um programa que leia varios números inteiros pelo teclado. 
+No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. 
+O programa deve perguntar ao usuário se ele quer ou não continuar a digitar  valor.
+'''
+
 media = soma = cont = 0
 p = 'S'
 lista = []
