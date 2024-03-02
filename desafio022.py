196bd33adb0d7980979bfbf2c506be71ddd71186
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:42:22 -0300

    Update desafio022.py

--- desafio022.py
+++ desafio022.py
@@ -1,3 +1,11 @@
+'''
+Desafio 022:  Crie um programa que leia o nome completo de uma pessoa e mostre:          
+→ O nome com todos as letras maiúsculas.   
+→ O nome com todas as letras minúsculas.       
+→ Quantas letras ao todo (sem considerar espaços). 
+→ Quantas letras tem o primeiro nome.
+'''
+
 nome = str(input('Digite seu nome completo: ')).strip()
 print('Analisando seu nome...')
 print('Seu nome em maiúsculas é: {}'.format(nome.upper()))
