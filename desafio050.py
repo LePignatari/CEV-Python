af714688b4ac1a6c8f7da4c358944aab44b38280
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 19:06:12 -0300

    Update desafio050.py

--- desafio050.py
+++ desafio050.py
@@ -1,6 +1,11 @@
+'''
+Desafio 050 - Desenvolva um programa que leia seis números inteiros e mostre a soma apenas daqueles que forem pares. Se o valor digitado for ímpar, desconsidere-o.
+'''
+
 print('-=-' * 10)
 print(F'{' ':>8}TERMOS PARES')
 print('-=-' * 10)
+
 cont = 0
 soma = 0
 for p in range(1, 7):
@@ -8,4 +13,5 @@ for p in range(1, 7):
     if n % 2 == 1:
         soma += n
         cont += +1
+        
 print(f'A soma dos {cont} termos pares foi de: {soma}')
