48a7fddc6475da554ff8736ff09340cadea90acc
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:56:41 -0300

    Update desafio039.py

--- desafio039.py
+++ desafio039.py
@@ -1,10 +1,18 @@
+'''
+Desafio 039: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com sua idade: 
+→ Se ele ainda vai se alistar ao serviço militar, Se é a hora de se alistar, Se já passou do tempo do alistamento. 
+Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+''' 
+
 from datetime import date
 print('\033[31m-=-'*6)
 print('DIA DO ALISTAMENTO')
 print('-=-'*6)
+
 ano = int(input('\033[mData de nascimento: '))
 hoje = date.today().year
 i = hoje - ano
+
 print(f'Você nasceu em {ano} e tem {i} em {hoje}, portanto:')
 if i < 18:
     print('Ainda \033[31mnão pode\033[m fazer o alistamento.')
