46fe328afb994e7a37db888d4582ffef31537efa
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 19:21:37 -0300

    Update desafio067.py

--- desafio067.py
+++ desafio067.py
@@ -1,3 +1,8 @@
+'''
+Desafio 067: Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitada pelo usuário. 
+O programa será interrompido quando o número solicitado for negativo. 
+'''
+
 while True:
     n = int(input('Qual valor deseja ver a tabuada?: '))
     cont = 1
