1706ff3082882dbe851a855cec04b57075c31042
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 19:05:22 -0300

    Update desafio048.py

--- desafio048.py
+++ desafio048.py
@@ -1,3 +1,7 @@
+'''
+Desafio 048 - Faça um programa que calcule a soma entre todos os números ímpares, que são multiplos de três, e que se encontram no intervalo de 1 até 500.
+'''
+
 soma = 0
 cont = 0
 for impar in range(1, 501, 2):  # definimos os números impares
