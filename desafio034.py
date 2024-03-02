cccdc595d0b70095a4ba467f7ab19fd86e14d102
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:51:32 -0300

    Update desafio034.py

--- desafio034.py
+++ desafio034.py
@@ -1,6 +1,12 @@
+'''
+Desafio 034: Escreva um programa que pergunte o salário de um funcionário e calcule o valor do seu aumento. 
+Para salários superiores a R$1.250,00, calcule um aumento de 10%. Para os inferiores ou iguais, o aumento é de 15%.
+'''
+
 print('-=-'*15)
 s = float(input('Digite o valor do seu salário: \033[32mR$\033[m'))
 print('-=-'*15)
+
 if s >= 1250:
     aum1 = (10 / 100) * s
     ns1 = aum1 + s
