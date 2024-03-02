89b730b57eee079f98e65007415a27ddfd9c1886
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 19:13:31 -0300

    Update desafio057.py

--- desafio057.py
+++ desafio057.py
@@ -1,7 +1,14 @@
+'''
+Desafio 057: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores “M” ou “F”. 
+Caso esteja errado, peça a digitação novamente até ter um valor correto.
+'''
+
 print('-=-' * 10)
 sexo = str(input('Olá! Por favor, informe o seu sexo \033[32m[M/F]\033[m: ')).strip().upper()[0]
+
 #while sexo != 'M' and sexo != 'F':    <<<< minha resolução inicial.
 while sexo not in 'FM':     # enquanto a resposta não estiver em masc e fem.   <<<< resolução do professor.
     sexo = str(input('\033[31mDado incorreto\033[m, por favor informe seu sexo novamente \033[32m[M/F]\033[m: ')).strip().upper()[0]
+    
 print(f'\033[32mSexo {sexo} registrado com êxito!\033[m')
 print('-=-' * 10)
