5dcd8d7696ccf81c06041a4717bf554cc9b33cd5
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:43:32 -0300

    Update desafio024.py

--- desafio024.py
+++ desafio024.py
@@ -1,3 +1,7 @@
+'''
+Desafio 024: Crie um programa que leia o nome de uma cidade e diga se ela começa ou não com nome “SANTO”.
+'''
+
 nome = str(input('Digite o nome de uma cidade: ')).strip()
 div = (nome.lower().split())
 print('A cidade começa com SANTO?: {}'.format('santo' in div[0]))
