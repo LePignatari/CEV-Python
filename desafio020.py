90554f8acf2056d28433744624714483485bcd45
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:40:19 -0300

    Update desafio020.py

--- desafio020.py
+++ desafio020.py
@@ -1,4 +1,10 @@
+'''
+Desafio 020: O mesmo professor do desafio anterior quer sortear a ordem de apresentação de trabalhos dos alunos. 
+Faça um programa que leia o nome dos quatro aluno e mostre a ordem sorteada.
+'''
+
 import random
+
 n1 = str(input('Digite o nome do primeiro aluno: '))
 n2 = str(input('Digite o nome do segundo aluno: '))
 n3 = str(input('Digite o nome do terceiro aluno: '))
