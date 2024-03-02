8727f6e9412593e84c695837615c01f23fa70ddc
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:49:01 -0300

    Update desafio031.py

--- desafio031.py
+++ desafio031.py
@@ -1,8 +1,15 @@
+'''
+Desafio 031: Desenvolva um programa que pergunte a distância de uma viagem em Km.
+Calcule o preço da passagem, cobrando R$0,50 por Km para viagens de até 200Km e R$0,45 para viagens mais longas.
+'''
+
 print('\033[36m-=-'*13)
 print('Viagens de até 200Km: R$0,50 por Km.')
 print('Viagens mais longas: R$0,45 por Km.')
 print('-=-'*13)
+
 d = int(input('\033[mQual a distância da viagem? '))
+
 if d <= 200:
     print(f'Para {d}Km o valor da passagem será de \033[32mR${d * 0.50:.2f}.')
 else:
