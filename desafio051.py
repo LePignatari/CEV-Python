216e548a6ebc9c4f2a241ff4690e6baef8213a2b
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 19:06:44 -0300

    Update desafio051.py

--- desafio051.py
+++ desafio051.py
@@ -1,9 +1,16 @@
+'''
+Desafio 051 - Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+'''
+
 print('-=-' * 10)
 print(f'{' ':>4}PROGRESSÃO ARITMÉTICA')
 print('-=-' * 10)
+
 p = int(input('Primeiro termo: '))
 r = int(input('Razão: '))
+
 for pa in range(1, 11):
     tg = p + (pa - 1) * r  # formula de pa.
     print(f'{tg} ⇨', end=' ')
+    
 print('ACABOU.')
