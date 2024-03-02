2ef2b3abb9407c7d8cf22c1ee1ef26eda075bf23
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:33:09 -0300

    Update desafio008-B.py

--- desafio008-B.py
+++ desafio008-B.py
@@ -1,3 +1,8 @@
+'''
+Desafio 008: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+>>> exercício com todas as conversões !
+'''
+
 v = float(input('Digite um valor em metros: '))
 print('A medida de {}m corresponde a: '.format(v))
 print('{}km'.format(v / 1000))
@@ -6,5 +11,3 @@ print('{}dam'.format(v / 10))
 print('{:.0f}dm'.format(v * 10))
 print('{:.0f}cm'.format(v * 100))
 print('{:.0f}mm'.format(v * 1000))
-
-
