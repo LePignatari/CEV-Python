69fcec6df350d75dead5481ab9af93c287006878
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:55:44 -0300

    Update desafio038.py

--- desafio038.py
+++ desafio038.py
@@ -1,8 +1,14 @@
+'''
+Desafio 038: Escreva um programa que leia dois números inteiros e compare-os, mostrando na tela um mensagem: 
+→ O primeiro valor é o maior, O segundo valor é o maior, Não existe valor maior, os dois são iguais.
+'''
+
 print('-=-'*10)
 n1 = int(input('Digite o primeiro valor: '))
 n2 = int(input('Digite o segundo valor: '))
 print('-=-'*10)
 print('Analisando os valores...')
+
 if n1 > n2:
     print('O \033[35mprimeiro valor\033[m é \033[32mMAIOR\033[m!')
 elif n2 > n1:
