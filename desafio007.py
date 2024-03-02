d4f0efa2cbd37e2ac11f7520d4b85a307c1bad57
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:31:34 -0300

    Update desafio007.py

--- desafio007.py
+++ desafio007.py
@@ -1,3 +1,7 @@
+'''
+Desafio 007: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+'''
+
 n1 = float(input('Digite sua primeira nota: '))
 n2 = float(input('Digite sua segunda nota: '))
 print('A média da suas duas notas é de: {:.1f}'.format((n1 + n2) / 2))
