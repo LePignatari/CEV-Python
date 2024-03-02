fa57043a475c725c45fc327da7c2a316ef153937
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:38:32 -0300

    Update desafio018.py

--- desafio018.py
+++ desafio018.py
@@ -1,3 +1,7 @@
+'''
+Desafio 018: Faça um programa que leia um ângulo qualquer e mostre na tela o valor do seno, cosseno e tangente desse ângulo.
+'''
+
 from math import cos, tan, sin, radians
 ang = float(input('Informe um ângulo: °'))
 print('O ângulo {} possui: '.format(ang))
