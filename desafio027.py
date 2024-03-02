6206881450d66da785b94ad239e9fd87079bead4
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:45:08 -0300

    Update desafio027.py

--- desafio027.py
+++ desafio027.py
@@ -1,5 +1,10 @@
+'''
+Desafio 027: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+'''
+
 nome = str(input('Escreva seu nome completo:')).strip()
 n = nome.split()
+
 print('Muito prazer em te conhecer!')
 print(f'\033[35mPrimeiro nome:\033[m {n[0]}')
 print(f'\033[35mÚltimo nome:\033[m {n[-1]}')  # print('Último nome: {}'.format(n[len(n)-1]))
