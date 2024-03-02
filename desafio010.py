af7bd98825336668c1c231f8ff97f14f0fadcc94
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:34:21 -0300

    Update desafio010.py

--- desafio010.py
+++ desafio010.py
@@ -1,5 +1,7 @@
+'''
+Desafio 010: Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar. (considere: R$3,27).
+'''
+
 d = float(input('Digite quanto dinheiro possui: R$'))
 dolar = d / 3.27
 print('Poderá comprar: US${:.2f} dólares.'.format(dolar))
-
-
