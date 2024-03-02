c5b38ee75a4c1d2f53b386d31225da6554a92a3a
Letícia <leticia.pignatari@gmail.com>
2024-03-02 17:56:50 -0300

    Exercício Python

---
+++ desafio033-B.py
@@ -0,0 +1,8 @@
+print('\033[35m|'*25)
+p = int(input('Digite o primeiro valor: '))
+s = int(input('Digite o segundo valor: '))
+t = int(input('Digite o terceiro valor: '))
+print('|'*25)
+num = [p, s, t]
+print(f'\033[4;33mO maior número é:\033[m \033[32m{max(num)}') # essa função já me entrega qual é o maior número.
+print(f'\033[4;33mO menor número é:\033[m \033[32m{min(num)}') # e essa o menor número.
