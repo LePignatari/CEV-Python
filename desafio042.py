2749d14d383c8d1d2b71b3e3ef1ab9a50d06ee46
Letícia Pignatari <126809352+LePignatari@users.noreply.github.com>
2024-03-02 18:59:25 -0300

    Update desafio042.py

--- desafio042.py
+++ desafio042.py
@@ -1,9 +1,19 @@
+'''
+Desafio 042: Refaça o desafio 035
+(Desafio 035: Desenvolva um programa que leia o comprimento de três retas e diga ao usuário se elas podem ou não formar um triângulo.)
+dos triângulos, acrescentando o recurso de mostrar que tipo de triângulo será formado: 
+→ Equilátero: todos os lados iguais 
+→ Isósceles: dois lados iguais 
+→ Escaleno: todos os lados diferentes.
+'''
+
 print('-=-' * 10)
 print('ANALISANDO O TRIÂNGULO')
 r1 = float(input('Valor da reta 1: '))
 r2 = float(input('Valor da reta 2: '))
 r3 = float(input('Valor da reta 3: '))
 print('-=-' * 10)
+
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('\033[32mPodemos\033[m formar um triângulo!')
     if r1 == r2 == r3:
